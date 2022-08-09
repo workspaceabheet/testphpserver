868b50e0d89c87f58ae31e89014199249b919e4f
Abheet Pandey <t-abpandey@microsoft.com>
2022-08-09 19:17:13 +0530

    9

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ $url = 'https://titanium9969-default-rtdb.firebaseio.com';
 /*$token = 'xxxxxxxxxxxxxxx';*/
 $token = 'FrR5M30YoQsL3ms82Ox5VxfkntfD7zeF5nFWzS8m';
 // --- Here is your parameter from the http GET
-$arduino_data = $_GET['arduino_data'];
+//$arduino_data = $_GET['arduino_data'];
 // --- $arduino_data_post = $_POST['name'];
 // --- Set up your Firebase url structure here
 $firebasePath = '/rt.json';
 /// --- Making calls
 $fb = new fireBase($url,$token);
-$response = $fb->get($firebasePath, $arduino_data);
+$response = $fb->get($firebasePath);
 sleep(2);
 ?>
 
