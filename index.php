c0cbdd3210246ac5a13182a8075dcd6005339baa
Abheet Pandey <t-abpandey@microsoft.com>
2022-08-09 19:16:26 +0530

    8

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 require_once 'site.php';
 // --- This is your Firebase URL
-$url = 'https://titanium9969-default-rtdb.firebaseio.com/';
+$url = 'https://titanium9969-default-rtdb.firebaseio.com';
 // --- Use your token from Firebase here
 /*$token = 'xxxxxxxxxxxxxxx';*/
 $token = 'FrR5M30YoQsL3ms82Ox5VxfkntfD7zeF5nFWzS8m';
@@ -10,10 +10,10 @@ $token = 'FrR5M30YoQsL3ms82Ox5VxfkntfD7zeF5nFWzS8m';
 $arduino_data = $_GET['arduino_data'];
 // --- $arduino_data_post = $_POST['name'];
 // --- Set up your Firebase url structure here
-$firebasePath = '/';
+$firebasePath = '/rt.json';
 /// --- Making calls
 $fb = new fireBase($url,$token);
-$response = $fb->push($firebasePath, $arduino_data);
+$response = $fb->get($firebasePath, $arduino_data);
 sleep(2);
 ?>
 
